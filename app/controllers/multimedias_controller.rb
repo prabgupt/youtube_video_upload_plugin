2288a2892fa840114d4e435fe55a299331cf7954
Prabhat Gupta <itisme.prabhat@gmail.com>
2011-05-13 23:48:48 +0530

    added non commercial license

--- app/controllers/multimedias_controller.rb
+++ app/controllers/multimedias_controller.rb
@@ -1,5 +1,21 @@
+#
+#
+#Copyright (c) 2011 Prabhat Gupta(prabhatgupta.webs.com | golygon.com)
+#
+#This script may be used for non-commercial purposes only. For any
+#commercial purposes, please contact the author at prabhat@golygon.com
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#OTHER DEALINGS IN THE SOFTWARE.
+#
+
 class MultimediasController < ApplicationController
-  # Require authentication only for edit and delete operation
    
   # GET /multimedias/new
   # GET /multimedias/new.xml
